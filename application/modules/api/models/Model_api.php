533015afdae3505a41fb18d200edff8f5d7a7470
nananghidayatulloh <nananghidayatulloh@gmail.com>
2022-05-27 21:46:39 +0700

    initial api module

--- application/modules/api/models/Model_api.php
+++ application/modules/api/models/Model_api.php
@@ -1,7 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 
-class Model_users extends CI_Model {
+class Model_api extends CI_Model {
 
  public function __construct() {
         parent::__construct();
